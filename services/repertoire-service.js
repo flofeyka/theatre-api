ba26790e87fc26e25fda2c02780b489853bc6ae9
Danil <flofey@yandex.com>
2024-12-07 00:24:47 +0500

    Images for repertoires added.

--- services/repertoire-service.js
+++ services/repertoire-service.js
@@ -1,9 +1,16 @@
 import { Op } from "sequelize";
 import ApiError from "../exceptions/api-error.js";
 import { Repertoire, Session } from "../models/models.js";
+import { v2 as cloudinary } from "cloudinary";
+
+cloudinary.config({
+  cloud_name: process.env.CLOUDINARY_NAME,
+  api_key: process.env.CLOUDINARY_API,
+  api_secret: process.env.CLOUDINARY_SECRET,
+});
 
 class repertoireService {
-  async addRepertoire({ title, description, category }) {
+  async addRepertoire({ title, description, category, image }) {
     if (category < 0 || category > 21 || isNaN(Number(category))) {
       throw ApiError.BadRequest("Неправильный формат возрастной категории");
     }
@@ -12,11 +19,25 @@ class repertoireService {
       description,
       title,
       category,
+      image,
     });
     return repertoire;
   }
 
   async editRepertoire(dto) {
+    const repertoireFound = await Repertoire.findOne({
+      where: { id: dto.id },
+    });
+
+    if (!repertoireFound) {
+      throw ApiError.NotFound("Репертуарная сцена не найдена");
+    }
+
+    if (repertoireFound.image) {
+      const publicId = existingUser.image.split('/').pop().split('.')[0];
+      await cloudinary.uploader.destroy(`images/${publicId}`)
+    }
+
     if (
       dto.category &&
       (dto.category < 0 || dto.category > 21 || isNaN(Number(dto.category)))
